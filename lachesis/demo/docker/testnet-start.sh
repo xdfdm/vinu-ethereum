732d18d625b1f05ead8282dba0f5bdfc1c372613
Andre Cronje <andre@fantom.foundation>
2019-03-08 09:52:48 +0200

    Lachesis testnet
    Signed-off-by: Andre Cronje <andre@fantom.foundation>

--- lachesis/demo/docker/testnet-start.sh
+++ lachesis/demo/docker/testnet-start.sh
@@ -4,16 +4,18 @@ set -ex
 
 cd $(dirname $0)
 
-N=2                                                                                                                                                                                                       
+N=5                                                                                                                                                                                                       
 
 for i in $(seq 1 $N)
 do
     docker create --cpus="0.3" --name=geth$i --net=lachesisnet --user $(id -u) geth-over-lachesis \
 	--networkid 1313 \
-	--rpc --rpcapi admin,eth,net,web3,personal,mine \
-	--lachesis=172.77.5.$i:1338 \
+	--rpc --rpcapi "admin,eth,net,web3,personal,mine,debug,txpool" \
+	--lachesis=172.31.29.148:9000 \
 	--verbosity 6 \
 	--nodiscover \
+	--ws --wsport 8546 --wsorigins="*" \
+	--syncmode "full" --gcmode "archive" \
 	--mine --miner.gasprice=0 --miner.threads=1 --ethash.dagdir=/gethdata
 
 # --fakepow
