3c116f2a92b560f6b676d7456b02016c6b26d1b6
devintegral2 <devintegral2@sfxdx.ru>
2019-11-27 10:12:38 +0300

    Replace ETH->FTM in maximum peer count string

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1111,7 +1111,7 @@ func SetP2PConfig(ctx *cli.Context, cfg *p2p.Config) {
 	if lightClient {
 		ethPeers = 0
 	}
-	log.Info("Maximum peer count", "ETH", ethPeers, "LES", lightPeers, "total", cfg.MaxPeers)
+	log.Info("Maximum peer count", "FTM", ethPeers, "LES", lightPeers, "total", cfg.MaxPeers)
 
 	if ctx.GlobalIsSet(MaxPendingPeersFlag.Name) {
 		cfg.MaxPendingPeers = ctx.GlobalInt(MaxPendingPeersFlag.Name)
